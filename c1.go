511dfd6f29d27c270dcf5f114b1d2551272e41d1
Aleksandar Dimov <isavitaisa@gmail.com>
2023-10-14 12:01:10 +0100

    Add day17

--- c1.go
+++ c1.go
@@ -1,71 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
+	steps := 343
+	buffer := []int{0}
+	currentPos := 0
+
+	for i := 1; i <= 2017; i++ {
+		currentPos = (currentPos + steps) % len(buffer)
+		buffer = append(buffer[:currentPos+1], buffer[currentPos:]...)
+		buffer[currentPos+1] = i
+		currentPos++
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	moves := strings.Split(scanner.Text(), ",")
-
-	programs := []rune("abcdefghijklmnop")
-
-	for _, move := range moves {
-		switch move[0] {
-		case 's':
-			x, _ := strconv.Atoi(move[1:])
-			spin(programs, x)
-		case 'x':
-			positions := strings.Split(move[1:], "/")
-			A, _ := strconv.Atoi(positions[0])
-			B, _ := strconv.Atoi(positions[1])
-			exchange(programs, A, B)
-		case 'p':
-			positions := strings.Split(move[1:], "/")
-			A := rune(positions[0][0])
-			B := rune(positions[1][0])
-			partner(programs, A, B)
+	for i, val := range buffer {
+		if val == 2017 {
+			fmt.Println(buffer[(i+1)%len(buffer)])
+			break
 		}
 	}
-
-	fmt.Println(string(programs))
-}
-
-func spin(programs []rune, x int) {
-	n := len(programs)
-	temp := make([]rune, n)
-	copy(temp, programs)
-
-	for i := 0; i < n; i++ {
-		programs[(i+x)%n] = temp[i]
-	}
-}
-
-func exchange(programs []rune, A int, B int) {
-	programs[A], programs[B] = programs[B], programs[A]
-}
-
-func partner(programs []rune, A rune, B rune) {
-	var indexA, indexB int
-	for i, p := range programs {
-		if p == A {
-			indexA = i
-		}
-		if p == B {
-			indexB = i
-		}
-	}
-	exchange(programs, indexA, indexB)
 }
--- c2.go
+++ c2.go
@@ -1,101 +1,21 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	defer file.Close()
+	steps := 343
+	currentPos := 0
+	valueAfterZero := 0
 
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	moves := strings.Split(scanner.Text(), ",")
-
-	programs := []rune("abcdefghijklmnop")
-	initial := string(programs)
-	cycleLen := 0
-
-	for i := 0; i < 1000000000; i++ {
-		for _, move := range moves {
-			switch move[0] {
-			case 's':
-				x, _ := strconv.Atoi(move[1:])
-				spin(programs, x)
-			case 'x':
-				positions := strings.Split(move[1:], "/")
-				A, _ := strconv.Atoi(positions[0])
-				B, _ := strconv.Atoi(positions[1])
-				exchange(programs, A, B)
-			case 'p':
-				positions := strings.Split(move[1:], "/")
-				A := rune(positions[0][0])
-				B := rune(positions[1][0])
-				partner(programs, A, B)
-			}
+	for i := 1; i <= 50000000; i++ {
+		currentPos = (currentPos + steps) % i
+		if currentPos == 0 {
+			valueAfterZero = i
 		}
-
-		if string(programs) == initial {
-			cycleLen = i + 1
-			break
-		}
+		currentPos++
 	}
 
-	programs = []rune(initial)
-	for i := 0; i < 1000000000%cycleLen; i++ {
-		for _, move := range moves {
-			switch move[0] {
-			case 's':
-				x, _ := strconv.Atoi(move[1:])
-				spin(programs, x)
-			case 'x':
-				positions := strings.Split(move[1:], "/")
-				A, _ := strconv.Atoi(positions[0])
-				B, _ := strconv.Atoi(positions[1])
-				exchange(programs, A, B)
-			case 'p':
-				positions := strings.Split(move[1:], "/")
-				A := rune(positions[0][0])
-				B := rune(positions[1][0])
-				partner(programs, A, B)
-			}
-		}
-	}
-
-	fmt.Println(string(programs))
-}
-
-func spin(programs []rune, x int) {
-	n := len(programs)
-	temp := make([]rune, n)
-	copy(temp, programs)
-
-	for i := 0; i < n; i++ {
-		programs[(i+x)%n] = temp[i]
-	}
-}
-
-func exchange(programs []rune, A int, B int) {
-	programs[A], programs[B] = programs[B], programs[A]
-}
-
-func partner(programs []rune, A rune, B rune) {
-	var indexA, indexB int
-	for i, p := range programs {
-		if p == A {
-			indexA = i
-		}
-		if p == B {
-			indexB = i
-		}
-	}
-	exchange(programs, indexA, indexB)
+	fmt.Println(valueAfterZero)
 }
