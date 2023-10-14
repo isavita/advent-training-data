99a348015180b5e8ef3a3dc657d9b4352b251521
Aleksandar Dimov <isavitaisa@gmail.com>
2023-10-14 11:05:49 +0100

    Add day12

---
+++ c1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func DFS(node int, adj map[int][]int, visited map[int]bool) {
+	visited[node] = true
+	for _, neighbor := range adj[node] {
+		if !visited[neighbor] {
+			DFS(neighbor, adj, visited)
+		}
+	}
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+	defer file.Close()
+
+	adj := make(map[int][]int)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " <-> ")
+		from, _ := strconv.Atoi(parts[0])
+		toNodes := strings.Split(parts[1], ", ")
+
+		for _, toNode := range toNodes {
+			to, _ := strconv.Atoi(toNode)
+			adj[from] = append(adj[from], to)
+			adj[to] = append(adj[to], from)
+		}
+	}
+
+	visited := make(map[int]bool)
+	DFS(0, adj, visited)
+
+	count := 0
+	for _, v := range visited {
+		if v {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
