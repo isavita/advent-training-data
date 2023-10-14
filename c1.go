ee1347ccde818d4dde0f25d2ec33117d9f988a7c
Aleksandar Dimov <isavitaisa@gmail.com>
2023-10-14 12:05:29 +0100

    Add day18

--- c1.go
+++ c1.go
@@ -1,25 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func main() {
-	steps := 343
-	buffer := []int{0}
-	currentPos := 0
+func getValue(arg string, registers map[string]int) int {
+	if val, err := strconv.Atoi(arg); err == nil {
+		return val
+	}
+	return registers[arg]
+}
 
-	for i := 1; i <= 2017; i++ {
-		currentPos = (currentPos + steps) % len(buffer)
-		buffer = append(buffer[:currentPos+1], buffer[currentPos:]...)
-		buffer[currentPos+1] = i
-		currentPos++
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var instructions [][]string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		instructions = append(instructions, strings.Fields(scanner.Text()))
 	}
 
-	for i, val := range buffer {
-		if val == 2017 {
-			fmt.Println(buffer[(i+1)%len(buffer)])
-			break
+	registers := make(map[string]int)
+	var lastSound int
+
+	for i := 0; i < len(instructions); {
+		instruction := instructions[i]
+		cmd := instruction[0]
+		arg1 := instruction[1]
+
+		switch cmd {
+		case "snd":
+			lastSound = getValue(arg1, registers)
+		case "set":
+			registers[arg1] = getValue(instruction[2], registers)
+		case "add":
+			registers[arg1] += getValue(instruction[2], registers)
+		case "mul":
+			registers[arg1] *= getValue(instruction[2], registers)
+		case "mod":
+			registers[arg1] %= getValue(instruction[2], registers)
+		case "rcv":
+			if getValue(arg1, registers) != 0 {
+				fmt.Println(lastSound)
+				return
+			}
+		case "jgz":
+			if getValue(arg1, registers) > 0 {
+				i += getValue(instruction[2], registers)
+				continue
+			}
 		}
+		i++
 	}
 }
