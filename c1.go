c109b29cf32eb2900b2d502b7eb4e46345aede95
Aleksandar Dimov <isavitaisa@gmail.com>
2023-10-14 11:31:10 +0100

    Add day15

--- c1.go
+++ c1.go
@@ -1,79 +1,44 @@
 package main
 
 import (
-	"encoding/hex"
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 )
 
-func reverseSection(arr []int, start, length int) {
-	n := len(arr)
-	for i, j := start, start+length-1; i < j; i, j = i+1, j-1 {
-		arr[i%n], arr[j%n] = arr[j%n], arr[i%n]
-	}
-}
-
-func knotHash(input string) string {
-	lengths := []int{}
-	for _, char := range input {
-		lengths = append(lengths, int(char))
-	}
-	lengths = append(lengths, 17, 31, 73, 47, 23)
-
-	list := make([]int, 256)
-	for i := range list {
-		list[i] = i
-	}
-
-	var position, skip int
-	for round := 0; round < 64; round++ {
-		for _, length := range lengths {
-			reverseSection(list, position, length)
-			position += length + skip
-			skip++
-		}
-	}
-
-	denseHash := make([]int, 16)
-	for i := 0; i < 16; i++ {
-		xor := 0
-		for j := 0; j < 16; j++ {
-			xor ^= list[i*16+j]
-		}
-		denseHash[i] = xor
-	}
-
-	hexHash := make([]byte, 16)
-	for i, v := range denseHash {
-		hexHash[i] = byte(v)
-	}
-	return hex.EncodeToString(hexHash)
-}
-
-func hexToBinary(hexStr string) string {
-	binaryStr := ""
-	for _, hexDigit := range hexStr {
-		val, _ := strconv.ParseUint(string(hexDigit), 16, 64)
-		binaryStr += fmt.Sprintf("%.4b", val)
-	}
-	return binaryStr
-}
-
 func main() {
-	keyString := "nbysizxe"
-	totalUsed := 0
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	defer file.Close()
 
-	for i := 0; i < 128; i++ {
-		rowKey := fmt.Sprintf("%s-%d", keyString, i)
-		hash := knotHash(rowKey)
-		binaryRow := hexToBinary(hash)
+	scanner := bufio.NewScanner(file)
 
-		for _, bit := range binaryRow {
-			if bit == '1' {
-				totalUsed++
-			}
+	scanner.Scan()
+	genAStart, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	scanner.Scan()
+	genBStart, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	genAFactor := int64(16807)
+	genBFactor := int64(48271)
+	modulus := int64(2147483647)
+
+	genA := genAStart
+	genB := genBStart
+	matches := 0
+
+	for i := 0; i < 40000000; i++ {
+		genA = (genA * genAFactor) % modulus
+		genB = (genB * genBFactor) % modulus
+
+		if genA&0xFFFF == genB&0xFFFF {
+			matches++
 		}
 	}
 
-	fmt.Println(totalUsed)
+	fmt.Println(matches)
 }
